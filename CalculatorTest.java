14a8910e40e4d5393f534b539a264fcbe2e9b06f
aryasubbu <104581993+aryasubbu@users.noreply.github.com>
2022-04-28 21:55:54 +0530

    adding second test case

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -4,12 +4,24 @@ import org.junit.platform.commons.annotation.Testable;
 
 @Testable
 public class CalculatorTest {
-
+	
+	
+	private calculatorMaster cal;
+	
+	public void init()
+	{
+		cal=new calculatorMaster();
+	}
 	public void emptyStringTestCase()
 	{
 
-		calculatorMaster cal=new calculatorMaster();
+		
 		assertEquals(cal.calcuate(""),0);
+	}
+	
+	public void singlevalue()
+	{
+		assertEquals(cal.calcuate("1"),1);
 	}
 
 }
