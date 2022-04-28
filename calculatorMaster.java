70a5b046216c2643f7368fd500441962061ab179
aryasubbu <104581993+aryasubbu@users.noreply.github.com>
2022-04-28 22:23:56 +0530

    updating master class to throw cusotm exception

--- calculatorMaster.java
+++ calculatorMaster.java
@@ -1,27 +1,35 @@
 
 public class calculatorMaster {
-
+ 
 	public static void main(String[] args) {
 
 
 	}
 
-	public Object calcuate(String string) {
+	public Object calcuate(String string) throws Exception {
 		String [] numbers=string.split(",|\n");
-      if(string.isEmpty())
-      {
+        if(string.isEmpty())
+         {
     	  return 0;
-      }else
-      {
+         }
+	else
+          {
 		return getsum(numbers);
-      }
-	}
+          }
+	 }
 
-	private int  getsum(String[] numbers) {
+	private int  getsum(String[] numbers) throws Exception {
+		
+		
 		int sum=0;
 		for(int i=0;i<numbers.length;i++)
 		{
+			if(Integer.parseInt(numbers[i])>0)
 			sum=sum+Integer.parseInt(numbers[i]);
+			else
+			{
+				throw new Exception("Negatives not allowed");
+			}
 		}
 		return sum;
 	}
