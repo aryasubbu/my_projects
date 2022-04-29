2152d78a8a0493fe70e136fe7e4ea73a397fa633
subhash <subhash@LAPTOP-F8HOSAK9>
2022-04-29 18:42:42 +0530

    commiting changes

--- String calculator/src/CalculatorTest.java
+++ String calculator/src/CalculatorTest.java
@@ -1,15 +1,46 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
 import org.junit.platform.commons.annotation.Testable;
 
 @Testable
 public class CalculatorTest {
 
-	public void emptyStringTestCase()
+	
+	private calculatorMaster cal;
+	
+	public void init()
+	{
+		cal=new calculatorMaster();
+	}
+	public void emptyStringTestCase() throws Exception
 	{
 
-		calculatorMaster cal=new calculatorMaster();
+		
 		assertEquals(cal.calcuate(""),0);
 	}
-
+	
+	public void singlevalue() throws Exception
+	{
+		assertEquals(cal.calcuate("1"),1);
+	}
+    
+	public void twonumbers() throws Exception
+	{
+		assertEquals(cal.calcuate("1,2"),3);
+	}
+	public void twonumbersnewlinedelimit() throws Exception
+	{
+		assertEquals(cal.calcuate("1\n2"),3);
+	}
+	public void threenumbersnewlinedelimit() throws Exception
+	{
+		assertEquals(cal.calcuate("1,2,3"),6);
+	}
+	
+	@Test(expected=Exception.class)
+	public void negativenumbers() throws Exception
+	{
+		cal.calcuate("-1");
+	}
 }
--- String calculator/src/calculatorMaster.java
+++ String calculator/src/calculatorMaster.java
@@ -1,14 +1,36 @@
 
 public class calculatorMaster {
-
+ 
 	public static void main(String[] args) {
 
 
 	}
 
-	public Object calcuate(String string) {
+	public Object calcuate(String string) throws Exception {
+		String [] numbers=string.split(",|\n");
+      if(string.isEmpty())
+      {
+    	  return 0;
+      }else
+      {
+		return getsum(numbers);
+      }
+	}
 
-		return -1;
+	private int  getsum(String[] numbers) throws Exception {
+		
+		
+		int sum=0;
+		for(int i=0;i<numbers.length;i++)
+		{
+			if(Integer.parseInt(numbers[i])>0)
+			sum=sum+Integer.parseInt(numbers[i]);
+			else
+			{
+				throw new Exception("Negatives not allowed");
+			}
+		}
+		return sum;
 	}
 
 }
